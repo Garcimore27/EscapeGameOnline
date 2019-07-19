2c12cb24f6cdcd69c30d20a750bba354ec2b6648
Garcimore27 <jose.garnil27@gmail.com>
2019-07-19 19:02:24 +0200

    Creation classe Joueur
    Contient les comparaisons à faire entre les différentes propositions

---
+++ src/Jeu/Joueur.java
@@ -0,0 +1,88 @@
+package Jeu;
+
+import java.util.List;
+
+public class Joueur {
+	
+	Combinaison creeCombi;
+	boolean gagne = false;
+	
+	
+	public Joueur(int nbChiffres){
+		creeCombi = new Combinaison(nbChiffres);
+	}
+	
+	/**
+	 * Méthode qui compare la proposition du joueur à la combinaison générée par l'ordinateur qu'il doit trouver 
+	 * @param proposition Proposition du Joueur
+	 * @return retourne une suite de +, - ou = en fonctions des chiffres à trouver et de la proposition du joueur
+	 */
+	
+	public String Compare(String proposition) {
+		String reponse = "";
+		int chiffresOK = 0;
+		gagne = false;
+		if (proposition.length() != this.creeCombi.getGenereCombi().size()) {
+			System.out.println("Désolé, la longueur proposée n'est pas la bonne, ceci est compté comme un essai quand même ");
+		}
+		// Si la proposition est trop longue, on ne prend que les X premiers car. correspondant à la taille de la combinaison à trouver
+		if (proposition.length() > this.creeCombi.getGenereCombi().size()) {
+			proposition = proposition.substring(0,this.creeCombi.getGenereCombi().size());
+			System.out.println("Seuls les " + this.creeCombi.getGenereCombi().size() + " premiers chiffres de votre proposition sont pris en compte ! (" + proposition + ")");
+		}
+		
+		// Si la proposition n'est pas assez longue, on la comble avec des zéros devant !
+		if (proposition.length() < this.creeCombi.getGenereCombi().size()) {
+			for (int i = 0; i < this.creeCombi.getGenereCombi().size() - proposition.length() + 1; i++) {
+				proposition = "0" + proposition;
+			}
+			System.out.println("Votre proposition a été modifiée en ceci : " + proposition + " pour être évaluée correctement !");
+		}
+		for (int i =0; i < this.creeCombi.getGenereCombi().size(); i++) {
+			if(this.creeCombi.getGenereCombi().get(i) > Character.getNumericValue(proposition.charAt(i))) {
+				reponse+= "+";
+			}else
+				if(this.creeCombi.getGenereCombi().get(i) < Character.getNumericValue(proposition.charAt(i))) {
+					reponse += "-";
+				} else {
+					reponse+= "=";
+					chiffresOK ++;
+				}
+		}
+		if(this.creeCombi.getGenereCombi().size() == chiffresOK) {
+			gagne = true;
+		}
+		return reponse;
+	}
+	
+	public boolean nouvelleCombinaison(String plusMoins, List<Integer> proposition) {
+
+		int chiffresOK = 0;
+		gagne = false;
+		
+		for (int i = 0; i < plusMoins.length(); i++) {
+			int chiffreEnCours = proposition.get(i);
+			int maxEnCours = this.creeCombi.getCombiMax().get(i);
+			int minEnCours = this.creeCombi.getCombiMin().get(i);
+			switch (plusMoins.charAt(i)) {
+				case '+':
+					this.creeCombi.getPropositionCombi().set(i, (int) ((maxEnCours + chiffreEnCours) / 2));
+					this.creeCombi.getCombiMin().set(i, (int) (chiffreEnCours));
+					break;
+				case '-':
+					this.creeCombi.getPropositionCombi().set(i, (int) ((minEnCours + chiffreEnCours) / 2));
+					this.creeCombi.getCombiMax().set(i, (int) (chiffreEnCours));
+					break;
+				case '=':
+					this.creeCombi.getCombiMax().set(i, (chiffreEnCours));
+					this.creeCombi.getCombiMin().set(i, (chiffreEnCours));
+					chiffresOK++;
+					break;
+			}
+		}
+		if (chiffresOK == proposition.size()) {
+			gagne = true;
+		}
+		return gagne;
+	}
+}
